b16ef1f1a5d941a0cb326cf38acdb2c0a8e259c5
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-01-27 12:14:14 +0100

    updates

--- test/journaledstrings/journaledstrings.jl
+++ test/journaledstrings/journaledstrings.jl
@@ -111,7 +111,7 @@ tree.children[node_name] = new_node
 end
 
 
-function flatten(tree::JSTree, node_name::String)::String
+function flatten(tree::JSTree, node_name::String)
     # Base case: Root sequence
     if node_name == "root"
         return tree.root
@@ -119,7 +119,7 @@ function flatten(tree::JSTree, node_name::String)::String
 
     # Recursive case: Flatten parent and apply deltas
     node = tree.children[node_name]
-    parent_sequence = flatten(tree, node.parent)
+    parent_sequence = flatten(tree, node.parent.name)
     return apply_delta(parent_sequence, node.deltaMap)
 end
 
@@ -140,7 +140,10 @@ end
 function print_sequences(tree::JSTree)
     println("root: ", tree.root)
     for (name, node) in tree.children
-        println("$name: ", flatten(tree, node.name))
+        if name != "root"
+        println("$name: ")
+        println(flatten(tree, node.name))
+        end
     end
 end
 
--- test/journaledstrings/run_test.jl
+++ test/journaledstrings/run_test.jl
@@ -5,10 +5,6 @@ reference_seq = LongDNA{4}("AGATCGAGCGAGCTAGCGACTCAG")
 # Initialize the deltaMap for how many sequences you want
 deltaMap = [SortedDict{Int, JournalEntry}() for _ in 1:10]  # Ten subseqs
 
-println(typeof(reference_seq))  # Expected: LongDNA{4}
-println(typeof(deltaMap))       # Expected: Vector{SortedDict{Int, JournalEntry}}
-println(typeof(deltaMap[1]))    # Expected: SortedDict{Int, JournalEntry}
-
 # Create a JournaledString
 jst = JournaledString(reference_seq, deltaMap)
 
@@ -18,12 +14,12 @@ println("Prova di: INS e SNP")
 add_delta!(jst, [1, 2], DeltaTypeIns, 8, "CGTA")
 add_delta!(jst, [4, 9], DeltaTypeSnp, 10, 'C')
 add_delta!(jst, [8], DeltaTypeIns, 24, "NNNNN")
-print_sequences(jst)
+# print_sequences(jst)
 
 println("\n")
 println("Prova di: DELETE (stringa 3 perde 10 caratteri)")
 add_delta!(jst, [3], DeltaTypeDel, 1, 10)
-print_sequences(jst)
+# print_sequences(jst)
 
 println("\n")
 println("Prova di: structure_variation")
@@ -36,7 +32,7 @@ add_delta!(jst, [7], DeltaTypeSV, 24, dna"NNNNN")
 add_delta!(jst, [7], DeltaTypeSV, 24, dna"NNNNN")
 
 # Print the sequences and the deltas
-print_sequences(jst)
+# print_sequences(jst)
 
 println("\n")
 println("Prova di: CNV")
@@ -53,13 +49,14 @@ add_delta!(js2, [1, 2], DeltaTypeIns, 8, "CGTA")
 # Compare two JournaledString objects
 println(is_equal(js1, js2))
 
+
 tree = JSTree(LongDNA{4}("AGATCGAGCGAGCTAGCGACTCAG"))
 
 add_node(tree, "root", deltaMap[2], "child1")
 add_node(tree, "child1", deltaMap[1], "child2")
-
+add_node(tree, "child1", deltaMap[3], "child3")
+add_node(tree, "child1", deltaMap[4], "child4")
+add_node(tree, "child4", deltaMap[5], "child5")
 
 print_tree(tree, "root", 0)
-
-# Reconstruct and print sequences
-#print_sequences(tree)
+print_sequences(tree)
