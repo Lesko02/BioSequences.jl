1a4ee91ca3016b0b3ba228bf67f5d9f105734e99
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-01-31 13:54:01 +0100

    Update run_test.jl

--- test/journaledstrings/run_test.jl
+++ test/journaledstrings/run_test.jl
@@ -8,21 +8,21 @@ deltaMap = [SortedDict{Int, JournalEntry}() for _ in 1:10]  # Ten subseqs
 # Create a JournaledString
 jst = JournaledString(reference_seq, deltaMap)
 
-println(typeof(jst))
+# println(typeof(jst))
 # Example: Apply insertions, deletions, SNPs, etc.
-println("Prova di: INS e SNP")
+# println("Prova di: INS e SNP")
 add_delta!(jst, [1, 2], DeltaTypeIns, 8, "CGTA")
 add_delta!(jst, [4, 9], DeltaTypeSnp, 10, 'C')
 add_delta!(jst, [8], DeltaTypeIns, 24, "NNNNN")
 # print_sequences(jst)
 
 println("\n")
-println("Prova di: DELETE (stringa 3 perde 10 caratteri)")
+# println("Prova di: DELETE (stringa 3 perde 10 caratteri)")
 add_delta!(jst, [3], DeltaTypeDel, 1, 10)
 # print_sequences(jst)
 
-println("\n")
-println("Prova di: structure_variation")
+# println("\n")
+# println("Prova di: structure_variation")
 add_delta!(jst, [5], DeltaTypeSV, 30, dna"CGTACGTACGTACGTA")
 add_delta!(jst, [10], DeltaTypeSV, 18, dna"CGTACGTACGTACGTA")
 add_delta!(jst, [7], DeltaTypeSV, 24, dna"NNNNN")
@@ -34,12 +34,12 @@ add_delta!(jst, [7], DeltaTypeSV, 24, dna"NNNNN")
 # Print the sequences and the deltas
 # print_sequences(jst)
 
-println("\n")
-println("Prova di: CNV")
+# println("\n")
+# println("Prova di: CNV")
 add_delta!(jst, [1], DeltaTypeCNV, 1, (LongDNA{4}("AAAA"), 5))
 
-print_sequences(jst)
-print_deltas(jst)
+# print_sequences(jst)
+# print_deltas(jst)
 
 # Create two identical JournaledString objects
 js1 = JournaledString(LongDNA{4}("AGATCGAGCGAGCTAGCGACTCAG"), [SortedDict{Int, JournalEntry}() for _ in 1:10], 0)
@@ -58,5 +58,10 @@ add_node(tree, "child1", deltaMap[3], "child3")
 add_node(tree, "child1", deltaMap[4], "child4")
 add_node(tree, "child4", deltaMap[5], "child5")
 
-print_tree(tree, "root", 0)
-print_sequences(tree)
+# print_tree(tree, "root", 0)
+# print_sequences(tree)
+
+add_delta!(js1, [10], DeltaTypeCNV, 6, (LongDNA{4}("AGATC"), 8))
+print_sequences(js1)
+pattern = LongDNA{4}("AGATC")
+naive_search(js1,pattern)
